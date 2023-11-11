f997913babec60ce9cdbcad73e3e5aabfef551c8
LimAiLin <limailin2009@gmail.com>
2023-11-11 00:31:06 -0500

    remove one test

--- frontend/src/components/common/Tags.test.jsx
+++ frontend/src/components/common/Tags.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Tags from "./Tags";
-import { toHaveStyle } from '@testing-library/jest-dom';
+import { getAllByText } from '@testing-library/jest-dom';
 
 describe("Tags Component", () => {
   it("should render a list of tags with correct labels", () => {
@@ -28,22 +28,5 @@ describe("Tags Component", () => {
     // Assert that no tags are rendered
     const tagElements = screen.queryAllByRole("listitem");
     expect(tagElements).toHaveLength(0);
-  });
-
-  it("should render tags with the correct styles", () => {
-    const tags = [
-      { name: "Tag1" },
-      { name: "Tag2" },
-      { name: "Tag3" },
-    ];
-
-    render(<Tags tags={tags} />);
-
-    // Assert that each tag has the correct CSS style
-    tags.forEach((tag) => {
-      const tagElement = screen.getByText(tag.name);
-      expect(tagElement).toHaveStyle("ml: 1");
-      expect(tagElement).toHaveStyle("color: primary.contrastText");
-    });
   });
 });
